21a493b5d7f9a3dda98d6747d3db0407d5f649f5
kruby <ts@kruby.dk>
2021-11-02 20:02:44 +0100

    Godt på vej med Boostrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,27 +3,37 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import 'bootstrap';
-import "../stylesheets/application";
+// import 'bootstrap';
+// import "../stylesheets/application";
+//
+// import Rails from "@rails/ujs";
+// import Turbolinks from "turbolinks";
+// import * as ActiveStorage from "@rails/activestorage";
+// import "channels";
+// require("@rails/ujs").start()
+// require("turbolinks").start()
+// require("@rails/activestorage").start()
+// require("channels")
+// require("jquery")
+// require("jquery-ui")
+// require("fancybox")
+// require("jquery_ujs")
+// require("ckeditor/init")
+// require("bootstrap")
+// require("flowtype")
+// require("moment")
+// require("moment/da.js")
+// require("bootstrap-datetimepicker")
+//
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
 
-import Rails from "@rails/ujs";
-import Turbolinks from "turbolinks";
-import * as ActiveStorage from "@rails/activestorage";
-import "channels";
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("jquery")
-require("jquery-ui")
-require("fancybox")
-require("jquery_ujs")
-require("ckeditor/init")
-require("bootstrap")
-require("flowtype")
-require("moment")
-require("moment/da.js")
-require("bootstrap-datetimepicker")
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import "../js/bootstrap_js_files.js"
 
 Rails.start()
 Turbolinks.start()
